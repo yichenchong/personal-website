b371287ed4231262025bc3065b5496bed09371bc
yichen <ycc21@ic.ac.uk>
2022-08-11 23:12:37 +0800

    icon structure update

--- helper/resume_loader/skills/skill_writer.py
+++ helper/resume_loader/skills/skill_writer.py
@@ -17,10 +17,11 @@ def clear():
     con.commit()
 
 def loadall():
+    release = "1.0.0"
     load("Computer Science", "https://cdn.jsdelivr.net/gh/yichenchong/elegant-circles/svg/dev.svg")
     load("Mathematics", "https://cdn.jsdelivr.net/gh/yichenchong/elegant-circles/svg/circlecompass.svg")
     load("Data", "https://cdn.jsdelivr.net/gh/yichenchong/elegant-circles/svg/trends.svg")
-    load("Robotics", "https://cdn.jsdelivr.net/gh/yichenchong/personal-website/static/img/robot.svg")
+    load("Robotics", f"https://cdn.jsdelivr.net/gh/yichenchong/personal-website/helper/resume_loader/skills/img/robot.svg")
     load("Python", "https://cdn.cdnlogo.com/logos/p/3/python.svg")
     load("Java", "https://cdn.cdnlogo.com/logos/j/2/java.svg")
     load("C", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg")
@@ -35,10 +36,10 @@ def loadall():
     load("CSS", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg")
     load("SQL", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg")
     load("Git", "https://cdn.cdnlogo.com/logos/g/15/git-icon.svg")
-    load("Flask", "https://cdn.jsdelivr.net/gh/yichenchong/personal-website/static/img/flask-original.png")
+    load("Flask", "https://cdn.jsdelivr.net/gh/yichenchong/personal-website/helper/resume_loader/skills/img/flask-original.png")
     load("Pytest", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytest/pytest-original.svg")
-    load("Swagger", "https://cdn.jsdelivr.net/gh/yichenchong/personal-website/static/img/swagger_logo.svg")
-    load("Postman", "https://cdn.jsdelivr.net/gh/yichenchong/personal-website/static/img/postman_logo.png")
+    load("Swagger", "https://cdn.jsdelivr.net/gh/yichenchong/personal-website/helper/resume_loader/skills/img/swagger_logo.svg")
+    load("Postman", "https://cdn.jsdelivr.net/gh/yichenchong/personal-website/helper/resume_loader/skills/img/postman_logo.png")
     load("Jira", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg")
     load("Arduino", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original.svg")
     load("JetBrains", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jetbrains/jetbrains-original.svg")
