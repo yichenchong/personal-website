67e3c68f574ba927eae90f9d6b5b9946112c86c9
yichen <ycc21@ic.ac.uk>
2022-09-11 23:57:02 +0800

    Loaded project data

--- data/layer_one/io/writer.py
+++ data/layer_one/io/writer.py
@@ -5,6 +5,7 @@ import uuid
 cwd = os.path.dirname(os.path.realpath(__file__))
 db_dir = os.path.join(os.path.split(cwd)[0], 'db')
 html_dir = os.path.join(os.path.split(cwd)[0], 'static/html')
+project_dir = os.path.join(os.path.split(cwd)[0], 'static/project_html')
 
 class ProjectWriter:
 
@@ -22,23 +23,26 @@ class ProjectWriter:
     
     @classmethod
     def setup(cls):
-        cls.cur.execute('''CREATE TABLE IF NOT EXISTS projects ( uuid TEXT UNIQUE NOT NULL, seq INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, subtitle TEXT, dates TEXT, content_page TEXT, featured TEXT );''')
-        cls.cur.execute('''CREATE TABLE IF NOT EXISTS project_skills ( row_id INTEGER PRIMARY KEY, project_uuid TEXT UNIQUE NOT NULL, skill_uuid TEXT );''')
+        cls.cur.execute('''CREATE TABLE IF NOT EXISTS projects ( uuid TEXT UNIQUE NOT NULL, seq INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE, subtitle TEXT, dates TEXT, content_page TEXT, featured TEXT );''')
+        cls.cur.execute('''CREATE TABLE IF NOT EXISTS project_skills ( row_id INTEGER PRIMARY KEY, project_uuid TEXT, skill_uuid TEXT );''')
         cls.cur.execute('''CREATE TABLE IF NOT EXISTS skills ( uuid TEXT UNIQUE NOT NULL, seq INTEGER PRIMARY KEY AUTOINCREMENT, skill_name TEXT, logo TEXT, featured INTEGER );''')
         cls.con.commit()
 
     @classmethod
     def add_project(cls, title, subtitle, dates, page_name, *skills):
-        proj_uuid = uuid.uuid4.hex()
-        with open(os.path.join(html_dir, f"{page_name}.html"), "x") as content_file:
+        proj_uuid = uuid.uuid4().hex
+        with open(os.path.join(project_dir, f"{page_name}.html"), "x") as content_file:
             content_file.write("")
         cls.cur.execute(f"INSERT INTO projects ( uuid, title, subtitle, dates, content_page, featured ) VALUES ( '{proj_uuid}', '{title}', '{subtitle}', '{dates}', '{page_name}', 0 )")
         for i in skills:
             cls.cur.execute(f"SELECT uuid FROM skills WHERE skill_name = '{ i }';")
             skill_uuid = cls.cur.fetchone()
             if skill_uuid is None:
-                skill_uuid = tuple(uuid.uuid4.hex())
-                cls.cur.execute(f"INSERT INTO skills ( uuid, skill_name, featured ) VALUES ('{skill_uuid[0]}', '{i}', 0 ")
+                skill_uuid = uuid.uuid4().hex
+                print(skill_uuid)
+                cls.cur.execute(f"INSERT INTO skills ( uuid, skill_name, featured ) VALUES ('{ skill_uuid }', '{i}', 0 );")
+            skill_uuid = skill_uuid[0] if isinstance(skill_uuid, tuple) else skill_uuid
+            cls.cur.execute(f"INSERT INTO project_skills ( project_uuid, skill_uuid ) VALUES ( '{proj_uuid}', '{skill_uuid}' );")
         cls.con.commit()
 
 
@@ -88,6 +92,166 @@ def disconnect():
 
 connect()
 ProjectWriter.setup()
+ProjectWriter.add_project(
+    "Innovation Institute Projects",
+    "Student/Participant",
+    "2015 - 2017",
+    "innovation_institute",
+    "Adobe Illustrator",
+    "Adobe Photoshop",
+    "Adobe Premiere",
+    "Arduino",
+    "C++",
+    "Design",
+    "Engineering",
+    "Robotics",
+    "Teamwork"
+)
+ProjectWriter.add_project(
+    "SASPX FRC Initiative",
+    "Founder",
+    "2016 - 2018",
+    "frc",
+    "Business",
+    "Engineering",
+    "Robotics"
+)
+ProjectWriter.add_project(
+    "DNA Digital Data Storage",
+    "Intel Science and Engineering Fair and AP Research Researcher",
+    "2017 - 2018",
+    "dna_digital_data_storage",
+    "Computer science",
+    "Python",
+    "Statistics"
+)
+ProjectWriter.add_project(
+    "AP Statistics Project",
+    "Student Research",
+    "2018",
+    "ap_stats_survey",
+    "CSS",
+    "Flask",
+    "HTML",
+    "Mathematics",
+    "Programming",
+    "Python",
+    "REST",
+    "SQL",
+    "Statistics",
+    "Teamwork",
+    "Web development"
+)
+ProjectWriter.add_project(
+    "Almanac",
+    "Co-Developer",
+    "2018 - 2019",
+    "almanac",
+    "Data",
+    "Flask",
+    "Python",
+    "SQL",
+    "Web scraping",
+    "Web development"
+)
+ProjectWriter.add_project(
+    "Vending Machine Committee",
+    "Lead Engineer",
+    "2018 - 2019",
+    "vending_machine",
+    "Adobe Photoshop",
+    "Business",
+    "C",
+    "C++",
+    "Computer science",
+    "Engineering",
+    "Mathematics",
+    "Physics",
+    "Robotics",
+    "Teamwork"
+)
+ProjectWriter.add_project(
+    "Projectile Rocket Simulation",
+    "Programmer",
+    "2020",
+    "projectile_rockets",
+    "C++",
+    "Computer science",
+    "Mathematics",
+    "Physics",
+    "Python"
+)
+ProjectWriter.add_project(
+    "Dynamic-R",
+    "Researcher/Author",
+    "2020",
+    "dynamic_r",
+    "Biology",
+    "Data",
+    "Mathematics",
+    "Statistics"
+)
+ProjectWriter.add_project(
+    "Mortality Predictions",
+    "Hobby project",
+    "2021",
+    "mortality_models",
+    "AI/ML",
+    "Data",
+    "Mathematics",
+    "Python",
+    "R"
+)
+ProjectWriter.add_project(
+    "Word Game Solvers",
+    "Hobby project",
+    "2021",
+    "word_games",
+    "C++",
+    "Java",
+    "Programming",
+    "Python"
+)
+ProjectWriter.add_project(
+    "Computing Practical 1 Labs",
+    "First-year curricular projects",
+    "2021 - 2022",
+    "comp40009",
+    "C",
+    "Computer science",
+    "Git",
+    "Haskell",
+    "Java",
+    "Kotlin",
+    "Programming"
+)
+ProjectWriter.add_project(
+    "Computing Practical 1 C Group Project",
+    "Building an ARM emulator and assembler",
+    "2021",
+    "c_group_project",
+    "Assembly",
+    "C",
+    "Computer science",
+    "Git",
+    "Programming",
+    "Teamwork"
+)
+ProjectWriter.add_project(
+    "Personal Website v1",
+    "Personal Portfolio Project (Work in Progress)",
+    "2022 - Present",
+    "personal_website_v1",
+    "CSS",
+    "Computer science",
+    "Flask",
+    "HTML",
+    "Programming",
+    "Python",
+    "SQL",
+    "Web development"
+)
+
 # ResumeWriter.add_item(
 #     category="Education",
 #     title="Shanghai American School",
