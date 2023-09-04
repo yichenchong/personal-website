8df80727eca396bccb061d8c0414e304e43a6508
yichenchong <yichenchong@yahoo.com>
2023-09-04 03:07:50 +0100

    Added new articles and sections from past year

--- data/layer_one/io/project_structure.py
+++ data/layer_one/io/project_structure.py
@@ -3,5 +3,5 @@ import os
 cwd = os.path.dirname(os.path.realpath(__file__))
 db_dir = os.path.join(os.path.split(cwd)[0], 'db')
 html_dir = os.path.join(os.path.split(cwd)[0], 'static/html')
-project_dir = os.path.join(os.path.split(cwd)[0], 'static/project_html')
+project_dir = os.path.join(os.path.split(cwd)[0], '../../blueprints/projects/templates')
 templates_dir = os.path.join(os.path.split(cwd)[0], 'static/templates')
